7e25af8301e1fbd13138da279ade7a4d309f054b
Dania Haroon <159708973+Daniaharoon@users.noreply.github.com>
2024-03-28 10:32:42 +0500

    Add files via upload

---
+++ ques23.js
@@ -0,0 +1,32 @@
+"use strict";
+//23. Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your prediction for the results of each test. Your code should look something like this:
+// // let car = 'subaru';
+// // console.log("Is car == 'subaru'? I predict True.")
+// // console.log(car == 'subaru')
+// // • Look closely at your results, and make sure you understand why each line evaluates to True or False.
+// // • Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru');
+//. Look closely at your results, and make sure you understand why each line evaluates to True or False.
+//. Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+let car1 = 'sabaru';
+//console.log("Is car == 'sabaru' ? I predict true otherwise I predict false.")//
+console.log(car1 === 'sabaru');
+console.log(car1 === 'civic');
+let name0 = 'Sara';
+//console.log("Its name0 == 'Sara' ? I predict true otherwise I predict false.")//
+console.log(name0 === 'Sara');
+console.log(name0 === 'Dua');
+let age = 18;
+//console.log(Its age == 23 ? I predict true other wise I predict false.")//
+console.log(age === 18);
+console.log(age === 22);
+let food = 'Biryani';
+//console.log("Its food == 'Biryani' ? I predict true other I predict false.")//
+console.log(food === 'Biryani');
+console.log(food === 'Pulao');
+let city = 'Kashmir';
+//console.log(Its city == 'Pakistan' ? I predict true otherwise I predict false.")//
+console.log(city === 'Kashmir');
+console.log(city === 'Hyderabad');
